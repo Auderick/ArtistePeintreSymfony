bb8d1db5c5de2e61d9b0ed89ec7e488dee1d941a
Christophe Guthoerl <contact@webtransform.fr>
2023-01-15 03:35:26 +0100

    création du portfolio

--- src/Repository/PeintureRepository.php
+++ src/Repository/PeintureRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Categorie;
 use App\Entity\Peinture;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -38,7 +39,7 @@ class PeintureRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-    // Pour récupérer les trois dernières penturesi
+    // Pour récupérer les trois dernières peintures
     /**
      * @return Peinture[] Returns an array of Peinture objects
      */   
@@ -51,28 +52,17 @@ class PeintureRepository extends ServiceEntityRepository
                     ->getResult();
     }
 
-//    /**
-//     * @return Peinture[] Returns an array of Peinture objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Peinture
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    // Pour récupérer les trois dernières peintures
+    /**
+     * @return Peinture[] Returns an array of Peinture objects
+     */
+    public function findAllPortfolio(Categorie $categorie): array
+    {
+        return $this->createQueryBuilder('p')
+                    ->where(':categorie MEMBER OF p.categorie')
+                    ->setParameter('categorie', $categorie)
+                    ->getQuery()
+                    ->getResult()
+        ;
+    }
 }
---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+// Permet d'appeler dans twig les catégories à nimporte quelle endroit, en utilisant " categorieNavbar " 
+
+namespace App\Twig;
+
+use App\Repository\CategorieRepository;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+
+class AppExtension extends AbstractExtension
+{
+    private $categorieRepository;
+
+    public function __construct(CategorieRepository $categorieRepository)
+    {
+        $this->categorieRepository = $categorieRepository;
+    }
+    
+    public function getFunctions() 
+    {
+        return [
+            new TwigFunction('categorieNavbar', [$this, 'categorie']),
+        ];
+    }
+    
+    public function categorie(): array
+    { 
+        return $this->categorieRepository->findAll();
+    }
+}
